e35090469ebeb17136f4a3cdd19a2a73f99e4e77
Umidjon Xusanov <161422490+Umidjon-24@users.noreply.github.com>
2026-04-03 22:56:18 +0500

    Add DigitalProduct class for downloadable items

---
+++ src/week10/problem6/DigitalProduct.java
@@ -0,0 +1,11 @@
+public class DigitalProduct extends TaxableProduct {
+    private String downloadUrl; 
+    public DigitalProduct(String name, double price, String downloadUrl) {
+        super(name, price);
+        this.downloadUrl = downloadUrl;
+    }
+    public void print() {
+        super.print();
+        System.out.println("Download URL: " + downloadUrl);
+    }
+}
