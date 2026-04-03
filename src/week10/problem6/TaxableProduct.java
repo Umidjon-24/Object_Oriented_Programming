9e12c4e3a807a40007e611b71f641b1903a30460
Umidjon Xusanov <161422490+Umidjon-24@users.noreply.github.com>
2026-04-03 22:54:31 +0500

    Implement TaxableProduct class with tax calculation

---
+++ src/week10/problem6/TaxableProduct.java
@@ -0,0 +1,13 @@
+public class TaxableProduct extends Product implements Taxable {
+    public TaxableProduct(String name, double price) {
+        super(name, price);
+    }
+    public double calculateTax() {
+        return getPrice() * TAX_RATE;
+    }
+    public void print() {
+        double tax = calculateTax();
+        double total = getPrice() + tax;
+        System.out.println("Product: " + super.toString() + ", Tax: $" + tax + ", Total: $" + total);
+    }
+}
