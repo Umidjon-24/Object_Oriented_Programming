a66b8e7f7c49d8c209e74675c03609aaa4ca86ce
Umidjon Xusanov <161422490+Umidjon-24@users.noreply.github.com>
2026-04-03 22:55:11 +0500

    add print method in TaxableProduct class
    Updated print method to improve output format.

--- src/week10/problem6/TaxableProduct.java
+++ src/week10/problem6/TaxableProduct.java
@@ -1,4 +1,5 @@
 public class TaxableProduct extends Product implements Taxable {
+    
     public TaxableProduct(String name, double price) {
         super(name, price);
     }
@@ -6,8 +7,9 @@ public class TaxableProduct extends Product implements Taxable {
         return getPrice() * TAX_RATE;
     }
     public void print() {
+        super.print();
         double tax = calculateTax();
         double total = getPrice() + tax;
-        System.out.println("Product: " + super.toString() + ", Tax: $" + tax + ", Total: $" + total);
+        System.out.println("  Tax: $" + tax + ", Total: $" + total);
     }
 }
